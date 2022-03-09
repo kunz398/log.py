0bc81993452d0dea9851d38c66f81974e99cb912
Kunal Singh <kunz.kunal@hotmail.com>
2022-03-09 18:43:56 +1200

    made function generic

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import datetime as dt
 
-def createLog():
+def createLog(message):
     todaysDate_Time = str(dt.datetime.today().strftime('%Y-%B-%d %I:%M:%S %p'))
     f = open("log.log", "a+")
-    f.write(todaysDate_Time + "\t Messages\n")
+    f.write(todaysDate_Time + "\t" + message + " \n")
     f.close()
 
 if __name__ == '__main__':
-    createLog()
-    
+    createLog("Message")
+
  ### 2022-March-09 06:35:26 PM        Messages
  
   
